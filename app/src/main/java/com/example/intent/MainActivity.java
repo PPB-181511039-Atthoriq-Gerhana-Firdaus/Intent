0a47272a0c7dd98f426f37e8e22a438ab77e3a86
atthoriqgerhana <atthoriq.gerhana.tif18@polban.ac.id>
2020-03-02 12:06:53 +0700

    init commit

---
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//    public void onClick(View view){
+//        startActivity(new Intent("com.example.intent.SecondActivity"));
+//    }
+
+        int request_Code = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        startActivityForResult(new Intent("com.example.intent.SecondActivity"), request_Code);
+    }
+
+    public void onActivityResult (int requestCode, int resultCode, Intent data){
+        if (requestCode == request_Code){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(this, data.getData().toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
